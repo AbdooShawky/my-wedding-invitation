a3e31e1c3f2d105c4794ed9af7a36c0fa3f66421
Abdoo Shawky <56520536+AbdooShawky@users.noreply.github.com>
2025-07-17 01:26:18 +0300

    init project

---
+++ src/config/config.js
@@ -0,0 +1,39 @@
+const config = {
+  data: {
+    title: "Wedding of Abdelrahman & Hanan", // This will be generated automatically
+    description:
+      "We are getting married and happily invite you to celebrate this special moment with us.", // This can be randomized later
+    groomName: "Abdelrahman",
+    brideName: "Hanan",
+    parentGroom: "Son of Mr. Groom & Mrs. Groom",
+    parentBride: "Daughter of Mr. Bride & Mrs. Bride",
+    date: "2025-08-12",
+    maps_url: "https://maps.app.goo.gl/hDTG7bDGDgRPeGfj7",
+    maps_embed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.035933069293!2d31.2867650114991!3d30.064504474808835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583fa1e1fbdf13%3A0xf09bac0f4c3e78d1!2s7%20sky%20hall%20in%20Vehicles%20House!5e0!3m2!1sen!2seg!4v1752701758434!5m2!1sen!2seg",
+    time: "7:00 - 11:00 ",
+    location: "7Sky, دار المركبات    ",
+    address: "القاهرة, مدينة نصر, تقاطع صلاح سالم ش السكة البيضاء داخل دار المركبات",
+    ogImage: "/images/og-image.jpg",
+    favicon: "/images/favicon.ico",
+    agenda: [
+      {
+        title: "حفل الزواج",
+        date: "2025-08-12",
+        startTime: "19:00",
+        endTime: "23:00",
+        location: "",
+        address: "القاهرة, مدينة نصر, تقاطع صلاح سالم ش السكة البيضاء داخل دار المركبات",
+      }
+    ],
+    audio: {
+      src: "/audio/Mahmoud-El-Esseily.mp3",
+      title: "Mahmoud-El-Esseily-El-Lella.mp3", 
+      autoplay: true,
+      loop: true
+    },
+   
+  }
+};
+
+export default config;
