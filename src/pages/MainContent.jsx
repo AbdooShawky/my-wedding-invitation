ac90c8ba4c537d3152f3be24f4cd7b1bdbfa98a4
Abdoo Shawky <56520536+AbdooShawky@users.noreply.github.com>
2025-08-01 02:13:16 +0300

    remove unwanted imports

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -1,8 +1,6 @@
 import Hero from '@/pages/Hero'
 import Events from '@/pages/Events'
-import Location from '@/pages/Location';
-import Wishes from '@/pages/Wishes';
-import Gifts from '@/pages/Gifts';
+
 
 // Main Invitation Content
 export default function MainContent() {
